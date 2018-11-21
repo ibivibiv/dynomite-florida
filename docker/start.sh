d49dee97dfd3994709acceb0d5c576f79d7135ca
Harold Hannon <haroldhannon@hotmail.com>
2018-11-21 01:25:51 -0600

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#trickery here to use the ordinal id to give that rackname for the config for dynomite
 export ORDINAL_ID=${HOSTNAME##*-} 
 export RACK_ID='us-east-'$ORDINAL_ID'b'
-envsubst '$RACK_ID' <conf/redis_single.yml | tee conf/redis_single.yml
-envsubst '${DYNO_TOKEN}' <conf/redis_single.yml | tee conf/redis_single.yml
-#Start redis server on 22122
-redis-server --port 22122 &
-#Start the local proxy for florida... comment this out if you did something else here or maybe you are starting python version?
-nodejs proxy.js &
-src/dynomite --conf-file=conf/redis_single.yml -v11
+envsubst '$RACK_ID' </config/redis_single.yml | tee /config/redis_single.yml
+envsubst '${DYNO_TOKEN}' </config/redis_single.yml | tee /config/redis_single.yml
+export DYNOMITE_FLORIDA_IP=$(getent hosts floridaservice | awk '{ print $1 }')
+dynomite -c /config/dynomite.yml

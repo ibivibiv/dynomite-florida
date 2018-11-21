018223a480ec73dc9d183258c7df07a52df5a075
Harold Hannon <haroldhannon@hotmail.com>
2018-11-21 12:57:36 -0600

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ORDINAL_ID=${HOSTNAME##*-} 
-export ORDINAL_LETTER=$(python alpha_parse $ORDINAL_ID)
+export ORDINAL_LETTER=$(python alpha_parse.py $ORDINAL_ID)
 export RACK_ID='us-east-1'$ORDINAL_LETTER
 envsubst '$RACK_ID' </config/dynomite_single.yml | tee /config/dynomite_single.yml
 #envsubst '${DYNO_TOKEN}' </config/redis_single.yml | tee /config/redis_single.yml

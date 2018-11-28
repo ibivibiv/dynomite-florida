ac0ac39c39bb280f5d5395a4ef37678cdf76c9b5
Harold Hannon <haroldhannon@hotmail.com>
2018-11-28 12:27:38 -0600

    Update alpha_parse.py

--- alpha_parse.py
+++ alpha_parse.py
@@ -1,7 +1,19 @@
 import sys
+import socket
 
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t']
+alpha = ['a','b','c','d']
 
-number = int(sys.argv[1])
+hostname = socket.gethostname()
 
-print(alpha[number])
+tokens = hostname.split('-')
+
+datacenter = hostname[:-2]
+
+rackid = datacenter + alpha[int(tokens[3])]
+
+with open('dynomite_single.yml', 'r') as myfile:
+    filestring = myfile.read().replace('$DATACENTER', datacenter).replace('$RACK_ID', rackid)
+
+file = open('dynomite_single.yml', 'w')
+file.write(filestring)
+file.close()

cf4631ee86f466fd8a82bd5ac4030c89b59b9d58
Harold Hannon <haroldhannon@hotmail.com>
2018-11-29 02:51:03 -0600

    Update alpha_parse.py

--- alpha_parse.py
+++ alpha_parse.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 
-alpha = ['a','b','c','d']
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 hostname = socket.gethostname()
 

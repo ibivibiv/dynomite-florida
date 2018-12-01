66db5a97ff1e8e789b5457f9f27178dde54cb24d
Harold Hannon <haroldhannon@hotmail.com>
2018-11-30 21:45:09 -0600

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,9 @@
 cd /dynomiteconfig
 wget https://raw.githubusercontent.com/ibivibiv/dynomite-florida/master/dynomite_single.yml
 python alpha_parse.py
-mv /dynomiteconfig/dynomite_single.yml /config/dynomite.yml
+envsubst '$RACK_ID' </dynomiteconfig/dynomite_single.yml | tee /dynomiteconfig/dynomite_single.yml
+envsubst '${DYNO_TOKEN}' </dynomiteconfig/dynomite_single.yml | tee /dynomiteconfig/dynomite_single.yml
+mv /dynomiteconfig/dynomite_single.yml /etc/dynomitedb/dynomite.yaml
 export DYNOMITE_FLORIDA_IP=$(getent hosts floridaservice | awk '{ print $1 }')
-dynomite -c /config/dynomite.yml 
+dynomite -c /etc/dynomitedb/dynomite.yaml
 
